ec5cc29cdd68b847e2d5f1106513df74e6195aea
MCWXT <2775997367@qq.com>
2025-01-05 11:20:08 +0800

    update

--- components/navBar.js
+++ components/navBar.js
@@ -1,11 +1,13 @@
-import { ref } from 'vue';
-import { getTemplate } from 'tao';
+import { ref, watch } from 'vue';
+import { getTemplate, cache } from 'tao';
 import themes from '/data/themes.js';
 export default {
   setup() {
-    
+    const currentTheme = ref(cache.getItem('currentTheme') || 'light');
+    watch(currentTheme, (theme) => cache.setItem('currentTheme', theme));
     return {
-      themes
+      themes,
+      currentTheme
     }
   },
   template: await getTemplate('/templates/components/navBar.html')
