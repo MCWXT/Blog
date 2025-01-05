6fd63ab18597df634bd666e9c6ac02c451693ad9
MCWXT <2775997367@qq.com>
2025-01-05 10:13:58 +0800

    update

--- components/navBar.js
+++ components/navBar.js
@@ -1,10 +1,11 @@
 import { ref } from 'vue';
 import { getTemplate } from 'tao';
+import themes from '/data/themes.js';
 export default {
   setup() {
     
     return {
-      
+      themes
     }
   },
   template: await getTemplate('/templates/components/navBar.html')
