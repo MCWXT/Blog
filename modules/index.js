08d487cb280d31770b132a0aef1bfd93776d6acd
MCWXT <2775997367@qq.com>
2025-01-13 19:50:45 +0800

    update index.js

---
+++ modules/index.js
@@ -0,0 +1,26 @@
+import { Axios } from 'axios';
+const axios = new Axios({});
+export const getTemplate = async (src) => {
+  const url = src + '.html';
+  return axios.get(url).then((response) => response.data);
+}
+export const httproxy = (url) => {
+  const proxyServer = 'https://proxy.mcwxt.top/';
+  return proxyServer + url;
+};
+export const cache = {
+  setItem(key, content) {
+    localStorage.setItem(key, typeof content == 'string' ? content : JSON.stringify(content));
+    return content;
+  },
+  getItem(key) {
+    const content = localStorage.getItem(key);
+    return content && content.indexOf('{') != -1 ? JSON.parse(content) : content;
+  },
+  removeItem(key) {
+    localStorage.removeItem(key);
+  },
+  clear() {
+    localStorage.clear();
+  }
+}
