7222c3d8713eee23ff2ec55689d85639f6a1826e
MCWXT <2775997367@qq.com>
2025-01-14 12:30:33 +0800

    update

--- functions/api/bilibili/video/[bvid].js
+++ functions/api/bilibili/video/[bvid].js
@@ -1,4 +1,4 @@
 export async function onRequestGet(context) {
-  const response = fetch('https://api.bilibili.com/x/web-interface/view?bvid=' + context.params.bvid);
+  const response = await fetch('https://api.bilibili.com/x/web-interface/view?bvid=' + context.params.bvid);
   return new Response(response.body);
 }
