51aaf94fd666eaeb1641a0170d7f6dd58346e0c2
MCWXT <2775997367@qq.com>
2025-01-14 12:21:01 +0800

    update

--- functions/api/bilibili/popular.js
+++ functions/api/bilibili/popular.js
@@ -1,4 +1,4 @@
 export function onRequestGet(context) {
   const result = fetch('https://api.bilibili.com/x/web-interface/popular').then(response => response.text());
-  return result;
+  return new Response(result);
 }
