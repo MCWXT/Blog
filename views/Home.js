10a0b82c41790689b5e585df61caef6e117268b1
MCWXT <2775997367@qq.com>
2025-07-13 14:16:14 +0800

    删除友链表，修复友链申请无法显示问题

--- views/Home.js
+++ views/Home.js
@@ -1,13 +1,11 @@
 import { ref } from 'vue';
 import { getTemplate, axios } from 'tao';
-import friendshipLink from '/assets/data/FriendshipLink.js';
 import LinkCard from '/components/LinkCard.js';
 export default {
   setup() {
     const issuesFriendLink = ref();
-    axios.get('https://storage.mcwxt.top/data/json/issues.json').then((response) => issuesFriendLink.value = JSON.parse(response.data));
+    axios.get('https://storage.mcwxt.top/data/json/issues.json').then((response) => issuesFriendLink.value = JSON.parse(response.data)).catch(() => issuesFriendLink.value = [{ title: '出问题啦', url: 'https://github.com/MCWXT/Blog/issues', icon: 'https://img.icons8.com/?size=100&id=k0I9K7HGGgQY&format=png&color=000000', brief: '提交错误'}]);
     return {
-      friendshipLink,
       issuesFriendLink
     }
   },
