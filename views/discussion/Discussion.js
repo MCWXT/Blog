83f10fc22390a22bd9edf02fda9e349b789f0782
MCWXT <2775997367@qq.com>
2025-08-29 10:36:22 +0800

    UI细节调整，更新giscus评论系统

--- views/discussion/Discussion.js
+++ views/discussion/Discussion.js
@@ -3,17 +3,15 @@ import { getTemplate, cache } from 'tao';
 import { useRoute } from 'vue-router';
 import { marked } from '/cdn_modules/marked@15.0.5/lib/marked.esm.min.js';
 import { octokit, config} from '/modules/server/github.js';
+import 'https://esm.sh/giscus';
 export default {
   setup() {
     const route = useRoute();
     const number = route.params.number;
     const discussion = ref({});
-    const comments = ref([]);
     octokit.request(`GET /repos/{owner}/{repo}/discussions/${number}`, config).then((response) => discussion.value = response.data);
-    octokit.request(`GET /repos/{owner}/{repo}/discussions/${number}/comments`, config).then((response) => comments.value = response.data);
     return {
       discussion,
-      comments,
       number,
       marked,
     }
