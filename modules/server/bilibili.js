fe7eccdef897d1897fadf73f1dae54a3134b9675
MCWXT <2775997367@qq.com>
2025-10-03 12:05:32 +0800

    update bilibili.js

--- modules/server/bilibili.js
+++ modules/server/bilibili.js
@@ -1,7 +1,7 @@
 import { Axios } from 'axios';
 import { httproxy } from 'tao';
 let baseURL;
-if (/.* /.test(location.hostname)) {
+if (/[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/g.test(location.hostname)) {
   baseURL = 'https://api.mcwxt.top';
 } else {
   baseURL = 'http://localhost:3000';
