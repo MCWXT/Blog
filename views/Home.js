6f1e2fd0bb977b2ba153b7d44994445815d5eec0
MCWXT <2775997367@qq.com>
2025-10-19 10:27:51 +0800

    update Home.js

--- views/Home.js
+++ views/Home.js
@@ -4,7 +4,20 @@ import LinkCard from '/components/LinkCard.js';
 export default {
   setup() {
     const issuesFriendLink = ref();
-    axios.get('https://storage.mcwxt.top/data/json/issues.json').then((response) => issuesFriendLink.value = JSON.parse(response.data)).catch(() => issuesFriendLink.value = [{ title: '出问题啦', url: 'https://github.com/MCWXT/Blog/issues', icon: 'https://img.icons8.com/?size=100&id=k0I9K7HGGgQY&format=png&color=000000', brief: '提交错误'}]);
+    axios.get('https://api.mcwxt.top/issues/friendlink').then((response) => JSON.parse(response.data)).then((data) => {
+      const _issuesFriendLink = [];
+      data.map((value, key) => {
+        if (value.issue_state === 'closed') {
+          return;
+        }
+        _issuesFriendLink[key] = {}
+        _issuesFriendLink[key].title = value.site_title;
+        _issuesFriendLink[key].brief = value.site_brief;
+        _issuesFriendLink[key].url = value.site_url;
+        _issuesFriendLink[key].icon = value.site_icon;
+      });
+      issuesFriendLink.value = _issuesFriendLink;
+    }).catch(() => issuesFriendLink.value = [{ title: '出问题啦', url: 'https://github.com/MCWXT/Blog/issues', icon: 'https://img.icons8.com/?size=100&id=k0I9K7HGGgQY&format=png&color=000000', brief: '提交错误'}]);
     return {
       issuesFriendLink
     }
