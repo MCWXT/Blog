43ac6cab78bee8dddd30eac868b0abc7e20c62a8
MCWXT <2775997367@qq.com>
2025-10-25 17:33:12 +0800

    更新日志添加翻页功能，修复了一些Bug

--- views/log/Commits.js
+++ views/log/Commits.js
@@ -1,15 +1,16 @@
 import { ref, watch } from 'vue';
-import { getTemplate } from 'tao';
+import { getTemplate, toast } from 'tao';
 import { octokit, config } from '/modules/server/github.js';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 export default {
   setup() {
     const route = useRoute();
+    const router = useRouter();
     const commits = ref();
     const page = ref();
-    watch(page, (value) => {
-      if (value < 0 || typeof +value !== 'number') {
+    watch(page, (value, oldValue) => {
+      if (value < 0 || typeof + value !== 'number') {
         return;
       }
       octokit.request(`GET /repos/{owner}/{repo}/commits?page=${page.value}`, config).then((response) => {
@@ -17,8 +18,19 @@ export default {
           toast({
             type: 'error',
             content: response.status,
-          })
+          });
+          return;
         }
+        if (response.data[0] == undefined) {
+          toast({
+            type: 'error',
+            content: '已经到顶了啦!!',
+          });
+          page.value = oldValue || 1;
+          return;
+        }
+        router.push({ query: { page: value }});
+        scrollTo(0, 0);
         commits.value = response.data;
       });
     });
